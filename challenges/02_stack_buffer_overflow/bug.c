3ebd72846b2c4e737f142ff283879325391100c3
최무헌 <271946661+choimoohun@users.noreply.github.com>
2026-09-18 17:49:38 +0900

    Merge pull request #26 from choimoohun/practice
    Practice

--- challenges/02_stack_buffer_overflow/bug.c
+++ challenges/02_stack_buffer_overflow/bug.c
@@ -63,9 +63,10 @@ static int tri_index(int i, int j) {
 
 /* 파스칼의 삼각형을 tri[] 에 채운다. */
 static void build_pascal(int *tri, int rows) {
-    for (int i = 0; i <= rows; i++) {
+    for (int i = 0; i < rows; i++) {                // <- 부등호만 `<=`에서 `<`로
         for (int j = 0; j <= i; j++) {
             int idx = tri_index(i, j);
+            // fprintf(stderr, "write i=%d j=%d idx=%d SIZE=%d\n", i, j, idx, SIZE);
             if (j == 0 || j == i) {
                 tri[idx] = 1;                         /* 양 끝은 1 */
             } else {
