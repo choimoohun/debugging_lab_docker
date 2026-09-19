b7f3adb975d3ee07c1dbd9da2f551577e5d6f1be
최무헌 <271946661+choimoohun@users.noreply.github.com>
2026-09-19 16:58:29 +0900

    Practice (#27)
    * Closes #10
    
    * Closes #11
    
    * Closes #12

--- challenges/06_null_deref/bug.c
+++ challenges/06_null_deref/bug.c
@@ -49,6 +49,7 @@ static void parse_headers(char *text, Headers *h) {
     for (char *line = strtok(text, "\n"); line != NULL; line = strtok(NULL, "\n")) {
         char *colon = strchr(line, ':');   
 
+        if (!colon) continue;
         *colon = '\0';                    
         char *key = line;
         char *val = skip_ws(colon + 1);
